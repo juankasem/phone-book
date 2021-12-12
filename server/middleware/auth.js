d207f30a717c9e7d9256a0bde5e500a312b5bc56
juankasem <juankasem982@gmail.com>
2021-12-12 17:41:35 +0300

    Refactor auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,27 +2,18 @@ const jwt = require('jsonwebtoken');
 
 const SECRETKEY = process.env.SECRETKEY || "supersecretKey";
 
-const auth = async (req, res) => {
+const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const isAuthenticated = token.length < 500;
 
-        let decodedData;
-
-        if (token && isCustomAuth) {      
-            decodedData = jwt.verify(token, secret);
-      
-            req.userId = decodedData?.id;
-          } else {
-            decodedData = jwt.decode(token);
-      
-            req.userId = decodedData?.sub;
+        if (token) {      
+            const userData = jwt.verify(token, secret);
+            req.userId = userData?._id;
           }   
 
         next();
-        
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
